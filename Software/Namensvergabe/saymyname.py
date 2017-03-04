24031dd64c070bb09405570c66387ccc3b4c1459
afandrich <anatolij.fandrich@uni-oldenburg.de>
2017-03-05 00:16:50 +0100

    DB Zugriffe reduziert - Code an einigen Stellen gekürzt

--- Software/Namensvergabe/saymyname.py
+++ Software/Namensvergabe/saymyname.py
@@ -17,7 +17,7 @@ def on_connect(client, userdata, flags, rc):
     logging.info('Verbindung zum Broker erfolgreich aufgebaut')
     logging.debug("Antwort vom Server: " + str(rc))
 
-    client.subscribe([("esp/sensor/mac/#", 2), ("esp/aktuator/mac/#", 2), ("esp/lastwill/mac/#", 2)])
+    client.subscribe([("esp/sensor/mac/#", 1), ("esp/aktuator/mac/#", 1), ("esp/lastwill/mac/#", 1)])
 
 def esp_last_will_callback(client, userdata, msg):
     logging.debug('Greetz aus dem last will callback')
@@ -27,15 +27,11 @@ def esp_last_will_callback(client, userdata, msg):
     last_will_payload = json.loads(last_will_data)
     last_will_mac_adr = last_will_payload['Mac']
 
+    #Setze Status auf Getrennt
+    c.execute("UPDATE espClients SET status = (?)  WHERE mac = (?)", ('Getrennt', last_will_mac_adr))
+    logging.info("Status von " + last_will_mac_adr + ": Getrennt.")
+    conn.commit()
 
-    c.execute('SELECT * FROM espClients')
-    for row in c.fetchall():
-        if last_will_mac_adr in row[1]:
-            c.execute("UPDATE espClients SET status = (?)  WHERE mac = (?)", ('Getrennt', last_will_mac_adr))
-
-            logging.info("Status von " + last_will_mac_adr + ": Getrennt.")
-            conn.commit()
-            break;
 
 def return_name_callback(client, userdata, msg):
     logging.debug('Greetz aus dem return name callback')
@@ -58,45 +54,53 @@ def return_name_callback(client, userdata, msg):
     except Exception as error:
         logging.error("JSON Fehler: " + str(error))
 
-    client_found_in_db = False
-    c.execute('SELECT * FROM espClients')
-    for row in c.fetchall():
-        logging.debug('DB Einträge: ' + str(row))
-        if esp_mac_adr in row[1]:
-            esp_new_name = row[4]
-            client_found_in_db = True
-            logging.info('Client ' + esp_name + ' anhand der Mac-Adresse wiedererkannt.')
-            c.execute("UPDATE espClients SET status = (?)  WHERE mac = (?)", ('Verbunden', esp_mac_adr))
-            logging.info("Status von " + esp_mac_adr + ": Verbunden.")
-            conn.commit()
 
-            break
-
-    if client_found_in_db == False:
+    #Prüfe, ob client mit der mac schon in db gelistet
+    c.execute("SELECT name FROM espClients WHERE mac = (?)", (client_payload['Mac'], ))
+    data = c.fetchone()
+    if data is None:
         logging.info('Mac-Adresse ' + esp_mac_adr + " bisher unbekannt.")
-        c.execute('SELECT * FROM espClients')
-        client_nr = 1
+        #Prüfe, wie oft der Sensor schon in der DB gelistet ist
+        logging.debug('Zähle Einträge mit dem Namen ' + client_payload['Name'])
+        anzahl = 0
+        c.execute("SELECT name FROM espClients WHERE type = (?)", (client_payload['Typ'],))
         for row in c.fetchall():
-            if esp_name in row[4]:
-               client_nr = client_nr + 1
-        logging.debug('Anzahl Namen: ' + str(client_nr))
-        esp_new_name = esp_name + str(client_nr)
+            if esp_name in row[0]:
+                anzahl = anzahl + 1
+        logging.debug(str(anzahl) + ' Einträge gefunden.')
+        #Lege neuen DB Eintrag an
+        esp_new_name = esp_name + str(anzahl)
         logging.debug('Neuer Name für Client: ' + esp_new_name)
         logging.info('Lege neuen DB Eintrag für ' + esp_new_name + ' an.')
         c.execute("INSERT INTO espClients (status, mac, IP, type, name) VALUES (? , ? , ?, ?, ? )",
                       (esp_status, esp_mac_adr, esp_ip_adr, esp_type, esp_new_name))
         conn.commit()
 
+
+
+    else:
+        logging.info('Client ' + esp_name + ' anhand der Mac-Adresse wiedererkannt.')
+        #Setze Status auf Verbunden
+        c.execute("UPDATE espClients SET status = (?) WHERE mac = (?)", ('Verbunden', esp_mac_adr,))
+        conn.commit()
+        #Suche den Namen aus der DB
+        logging.debug('Mac Adresse ' + esp_mac_adr + ' gefunden.')
+
+        logging.debug('Name des Clients in der DB: ' + data[0])
+        esp_new_name = data[0]
+
+
+
 #    c.close()
  #   conn.close()
 
     if esp_type == 'Sensor':
-        topic_str = 'nameClient/Sensor/mac/' + esp_mac_adr
-        client.publish(topic_str, esp_new_name, qos=2)
+        topic_str = 'nameClient/sensor/mac/' + esp_mac_adr
+        client.publish(topic_str, esp_new_name, qos=1)
         logging.info('Sensor ' + esp_name + ' erhält den Namen ' + esp_new_name)
     else:
         topic_str = 'nameClient/aktuator/mac/' + esp_mac_adr
-        client.publish(topic_str, esp_new_name, qos=2)
+        client.publish(topic_str, esp_new_name, qos=1)
         logging.info('Aktuator ' + esp_name + ' erhält den Namen ' + esp_new_name)
 
 
@@ -104,7 +108,10 @@ def create_table():
     c.execute('CREATE TABLE IF NOT EXISTS espClients( status TEXT, mac TEXT, IP TEXT, type TEXT, name TEXT )')
 
 
-client = mqtt.Client(client_id='name_client', clean_session=True)
+
+
+
+client = mqtt.Client(client_id='name_client123', clean_session=True)
 logging.debug('Client initialisiert')
 
 client.will_set("test/log", payload="Ich bin raus leute", qos=1, retain=False)

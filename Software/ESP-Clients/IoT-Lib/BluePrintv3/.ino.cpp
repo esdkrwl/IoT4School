ee7407fbccccd8f98df07e093647f3784d3b8879
afandrich <anatolij.fandrich@uni-oldenburg.de>
2017-03-09 19:19:28 +0100

    Eclipse Projekt

---
+++ Software/ESP-Clients/IoT-Lib/BluePrintv3/.ino.cpp
@@ -0,0 +1,15 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-03-09 19:06:34
+
+#include "Arduino.h"
+#include "Arduino.h"
+void setup() ;
+void loop() ;
+
+#include "BluePrintv3.ino"
+
+
+#endif
---
+++ Software/ESP-Clients/IoT-Lib/IoT-Library/src/IoT-Library.h
@@ -0,0 +1,15 @@
+/*
+ * IoT-Library.h
+ *
+ *  Created on: 09.03.2017
+ *      Author: a
+ */
+
+#ifndef IOT_LIBRARY_H_
+#define IOT_LIBRARY_H_
+
+
+
+
+
+#endif /* IOT_LIBRARY_H_ */

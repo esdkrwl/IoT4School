69275403087d8a6e8237fe36e9e1544032e26987
afandrich <anatolij.fandrich@uni-oldenburg.de>
2017-03-20 19:52:17 +0100

    nginx update

--- Software/Raspberry/nginx/mqttdb.php
+++ Software/Raspberry/nginx/mqttdb.php
@@ -1,40 +1,35 @@
+
+
 <?php
-
 		$file_db = new PDO('sqlite:/var/www/db/mqttClient.db');
-    	// Set errormode to exceptions
-    	$file_db->setAttribute(PDO::ATTR_ERRMODE, 
-                            PDO::ERRMODE_EXCEPTION);
 
-		    // Select all data from memory db messages table 
     	$result = $file_db->query('SELECT * FROM espClients');
 
-		//echo '
-        //	<tr>
-        //    	<td>'.'Status'.'</td>
-        //    	<td>'.'MAC'.'</td>
-		//		<td>'.'IP'.'</td>
-        //    	<td>'.'Typ'.'</td>
-		//		<td>'.'Name'.'</td>
-
-        //	</tr> 
-    	//	';
-		//echo('<br />');
-		//echo('<br />');
- 
+ 		$rows = null;
     	foreach($result as $row) {
-			echo '
-        		<tr>
-            		<td>'.$row["status"].'</td>
-            		<td>'.$row['mac'].'</td>
-            		<td>'.$row['IP'].'</td>
-          
-            		<td>'.$row['type'].'</td>
-            		<td>'.$row['name'].'</td>
-        		</tr> 
-    		';
-			echo('<br />');
-			echo('<br />');
+			$rows .= "<tr>";
+    		$rows .= "<td>{$row['status']}</td>";
+    		$rows .= "<td>{$row['mac']}</td>";
+    		$rows .= "<td>{$row['IP']}</td>";
+    		$rows .= "<td>{$row['type']}</td>";
+    		$rows .= "<td>{$row['name']}</td>";
+    		$rows .= "</tr>\n";
 		}
+
+		echo <<<EOD
+			<table>
+				<tr>
+    				<th>Status</th>
+    				<th>Mac</th>
+    				<th>IP</th>
+    				<th>Typ</th>
+    				<th>Name</th>
+				</tr>
+				$rows
+			</table>
+EOD;
 ?>
+
+
 
 

0bed785ea3e698f48bf92cbed630631661d21f84
Anatolij Fandrich <anatolij.fandrich@uni-oldenburg.de>
2018-07-28 18:18:36 +0200

    Update Binaries

--- Software/ESP-Clients/Aktor/Servo/Iot4School-Servo.ino
+++ Software/ESP-Clients/Aktor/Servo/Iot4School-Servo.ino
@@ -450,7 +450,7 @@ void connectToBroker() {
       wifiManager.addParameter(&custom_mqtt_port);
 
       wifiManager.setTimeout(180);
-      if (!wifiManager.startConfigPortal("MQTT-AP", "password")) {
+      if (!wifiManager.startConfigPortal("MQTT-AP")) {
         Serial.println("failed to connect and hit timeout");
         delay(3000);
         //reset and try again, or maybe put it to deep sleep
